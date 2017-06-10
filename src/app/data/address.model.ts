33cdaffbe30adf39735f81c211ab922af531967e
Lawrence Obrero <x119531@corp.ads>
2017-06-09 17:14:32 -0700

    Added form data

---
+++ src/app/data/address.model.ts
@@ -0,0 +1,3 @@
+export class Address {
+    constructor(public hostname: string, public ip: string, public location:string) {}
+}
--- src/app/new-request/source/source.component.ts
+++ src/app/new-request/source/source.component.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Address } from '../../data/address.model';
 
 @Component({
   selector: 'app-source',
@@ -11,6 +12,10 @@ export class SourceComponent implements OnInit {
 
   defaultLocation = "AB";
 
+  sourceAddress : Address[] = [];
+
+
+  next = false;
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -19,14 +24,17 @@ export class SourceComponent implements OnInit {
   }
 
 
-  onSubmit(form: NgForm) {
+  onAdd(form: NgForm) {
     console.log(form);
-
-    
-    
+    this.sourceAddress.push(new Address(form.value.hostname,form.value.ipsubnet,form.value.location));
+    console.log(this.sourceAddress);
+        console.log(this.sourceAddress.length);
+      if (this.sourceAddress.length > 0) this.next = true;
   }
 
   onNext() {
+
+    
     this.router.navigate(['/new/destination']);
   }
 
